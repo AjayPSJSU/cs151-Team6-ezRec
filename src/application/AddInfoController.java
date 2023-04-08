3fdaec1b8b0de49e88b4789cd0a968373ad41233
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2023-04-07 23:18:52 -0700

    Updated Homepage to include the addInfo files

---
+++ src/application/AddInfoController.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class AddInfoController {
+	
+	public void handleSubmit(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("Homepage.fxml"));
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
--- src/application/HomepageController.java
+++ src/application/HomepageController.java
@@ -75,4 +75,12 @@ public class HomepageController {
 		stage.show();
 	}
 	
+	public void addInfo(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("AddInfo.fxml"));
+		Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
 }
